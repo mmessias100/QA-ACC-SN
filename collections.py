0cc54ed4505beb1656f534accb78f12e4d6fa558
Morris Messias <messiasmorris@gmail.com>
2022-01-24 16:55:49 +0000

    third commit for an intro to python Collections

---
+++ collections.py
@@ -0,0 +1,20 @@
+# Python tutorial - Collections
+
+listvar1 = ["meat", "veg", "cake", "beer"]
+
+#            0       1       2       3
+#           -4      -3      -2      -1
+
+# print(listvar1)
+
+cool_cows = ["Winnie the Moo", "Moolan", "Milkshake", "Mooana"]
+
+print(cool_cows[1])
+
+dictvar1 = {"cow":"moo" ,"sheep":"baa" ,"chicken":"cluck" ,"dog":"bowwow"}
+print(dictvar1.items())
+print(dictvar1.keys())
+print(dictvar1.values())
+
+books = {"The Handmaiden's Tale":"Margaret Atwood", "The Hobbit":"J.R.R. Tolkien", "Charlie and the Chocolate Factory":"Roald Dahl"}
+print(books["The Hobbit"][3])
