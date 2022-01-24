92ead0980c8c653ea004817a2c46db36cf85a35c
Morris Messias <messiasmorris@gmail.com>
2022-01-24 16:55:15 +0000

    second commit data types

---
+++ data_types.py
@@ -0,0 +1,23 @@
+firstname = "Morris"
+lastname = 'Messias'
+var2 = 27.0
+var3 = True
+var4 = int(var2)
+
+print(firstname + " " + lastname)
+print(var2 , var4)
+# print(f"Who ate all the pies?")
+name = input("who ate all the pies? ")
+print(f"There goes {name} there goes {name} he ate all the pies")
+
+
+number1 = input("Enter whole number: ")
+number2 = input("Enter decimal number: ")
+
+integer_number = int(number1)
+float_number = float(number2)
+round_number = int(round(float_number))
+
+print(number1)
+print(number2)
+print(round_number)
