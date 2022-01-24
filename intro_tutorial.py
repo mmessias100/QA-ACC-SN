34e07ee6ea30d2538979825e68b3a2bf1c883bd3
Morris Messias <messiasmorris@gmail.com>
2022-01-24 16:54:46 +0000

    first commit intro

---
+++ intro_tutorial.py
@@ -0,0 +1,6 @@
+# name = input("Hello, what is your name? ")
+# age = int(input("and what is your age? "))
+
+firstname = "Morris"
+lastname = "Messias"
+print("Hello" + " " + firstname + " " + lastname)
